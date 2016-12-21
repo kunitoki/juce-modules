0fdc3db65bcc75d56a1752758d349d5471bc4529
lucio.asnaghi <lucio.asnaghi@king.com>
2016-12-21 22:13:09 +0100

    - added nanomsg module

---
+++ nanomsg/nanomsg.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+/*******************************************************************************
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               nanomsg
+  vendor:           nanomsg
+  version:          5.0.0
+  name:             nanomsg
+  description:      Socket library that provides several common communication patterns
+  website:          http://nanomsg.org/
+  license:          MIT
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+#include "src/nn.h"
+#include "src/bus.h"
+#include "src/pair.h"
+#include "src/pubsub.h"
+#include "src/reqrep.h"
+#include "src/inproc.h"
