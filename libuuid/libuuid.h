45407e5b819d258387c8d4cdd6f72471fc81e71f
lucio.asnaghi <lucio.asnaghi@king.com>
2016-12-21 22:16:12 +0100

    - added libuuid module (dependency of zeromq)

---
+++ libuuid/libuuid.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+/*******************************************************************************
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               libuuid
+  vendor:           slowfranklin
+  version:          1.0.3
+  name:             libuuid
+  description:      Portable uuid C library.
+  website:          https://sourceforge.net/projects/libuuid
+  license:          BSD
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+#include "dist/uuid.h"
