88f092b086d3e7e989f6e695ac591e418a2904e1
lucio.asnaghi <lucio.asnaghi@king.com>
2016-12-21 22:10:42 +0100

    - added catch unit test library

---
+++ catch/catch.h
@@ -0,0 +1,19 @@
+
+/*******************************************************************************
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               catch
+  vendor:           philsquared
+  version:          1.5.9
+  name:             Catch stands for C++ Automated Test Cases in Headers
+  description:      Catch stands for C++ Automated Test Cases in Headers.
+  website:          https://github.com/philsquared/Catch
+  license:          Boost Software License 1.0
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+//#define CATCH_CONFIG_RUNNER
+//#include "catch.hpp"
